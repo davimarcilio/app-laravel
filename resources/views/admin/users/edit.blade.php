8cd6a961cc96b98acc06303de358da4a088cb97e
Davi Marcilio <davimarcilio.js@gmail.com>
2024-07-27 18:08:56 -0300

    ♻️Refactor: form changed to a component

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -3,12 +3,8 @@
     
 @section('content')
  <h1>{{$user->name}}</h1>
-    <x-alert />
     <form action="{{route('users.update', $user->id)}}" method="POST">
-        @csrf()
         @method('put')
-        <input type="text" name="name" placeholder="Nome" value="{{$user->name}}" >
-        <input type="email" name="email" placeholder="E-mail" value="{{$user->email}}" >
-        <button type="submit">enviar</button>
+        @include('admin.users.partials.form')
     </form>
 @endsection
