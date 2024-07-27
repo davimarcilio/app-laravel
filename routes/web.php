3379b3656c3cd843779edc52a64166dbddebf0fa
Davi Marcilio <davimarcilio.js@gmail.com>
2024-07-27 18:47:46 -0300

    🐛Bug: `/users/create` invalidated by route order

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,12 @@ use App\Http\Middleware\CheckIfIsAdmin;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
     Route::delete('/users/{userId}/destroy', [UserController::class, 'destroy'])->name('users.destroy')->middleware(CheckIfIsAdmin::class);
     Route::get('/users/{userId}', [UserController::class, 'show'])->name('users.show');
     Route::put('/users/{userId}', [UserController::class, 'update'])->name('users.update');
     Route::get('/users/{userId}/edit', [UserController::class, 'edit'])->name('users.edit');
     Route::get('/users', [UserController::class, 'index'])->name('users.index');
-    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
     Route::post('/users', [UserController::class, 'store'])->name('users.store');
 });
 
